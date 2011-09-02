58450bd112a84ffc7ad34cb314d6a10215882794
Lukasz Harezlak <lukasz.harezlak@gmail.com>
2011-09-02 11:24:02 +0200

    Dices sorted out.

--- koans/about_dice_project.rb
+++ koans/about_dice_project.rb
@@ -1,10 +1,23 @@
 require File.expand_path(File.dirname(__FILE__) + '/edgecase')
 
 # Implement a DiceSet Class here:
-#
-# class DiceSet
-#   code ...
-# end
+
+ class DiceSet
+
+   def roll(amount)
+     i = 0
+     @dices = []
+     while i < amount do
+       @dices << (1 + rand(6))
+       i += 1
+     end
+   @dices
+   end
+ 
+   def values
+   @dices
+   end
+ end  
 
 class AboutDiceProject < EdgeCase::Koan
   def test_can_create_a_dice_set
